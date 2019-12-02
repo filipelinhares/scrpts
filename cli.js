59c712af245dba6c3f0f2b9e8847652d01821520
Roque Francisco <roquefsd@gmail.com>
2019-12-02 18:14:11 -0200

    v0.3.2

--- cli.js
+++ cli.js
@@ -6,38 +6,38 @@ const { bold } = require('./colors')
 const pkgPath = process.argv[2]
 
 const printScripts = file => {
-  const pkgInfo = pkgPath ? file[pkgPath] : file.scripts
+	const pkgInfo = pkgPath ? file[pkgPath] : file.scripts
 
-  if (!pkgInfo) {
-    console.log('Path not found in package.json')
-    process.exit(0)
-  }
+	if (!pkgInfo) {
+		console.log('Path not found in package.json')
+		process.exit(0)
+	}
 
-  const x = Object.keys(pkgInfo).map(f => {
-    return `• ${bold(f)}: ${pkgInfo[f]}`
-  })
+	const x = Object.keys(pkgInfo).map(f => {
+		return `• ${bold(f)}: ${pkgInfo[f]}`
+	})
 
-  return `${`\n${file.name}@${file.version} - Available scrpts:`}\n\n    ${x
-    .sort()
-    .join('\n    ')}\n`
+	return `${`\n${file.name}@${file.version} - Available scrpts:`}\n\n    ${x
+		.sort()
+		.join('\n    ')}\n\n`
 }
 
-async function main () {
-  const packagePath = path.resolve(process.cwd(), 'package.json')
+async function main() {
+	const packagePath = path.resolve(process.cwd(), 'package.json')
 
-  try {
-    const packageJson = await fs.readFile(packagePath, 'utf8')
-    const parsedPackageJson = JSON.parse(packageJson)
-    process.stdout.write(printScripts(parsedPackageJson))
-    process.exit(0)
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      process.stderr.write("You don't have a package.json in this folder")
-      process.exit(0)
-    } else {
-      throw new Error(error)
-    }
-  }
+	try {
+		const packageJson = await fs.readFile(packagePath, 'utf8')
+		const parsedPackageJson = JSON.parse(packageJson)
+		process.stdout.write(printScripts(parsedPackageJson))
+		process.exit(0)
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			process.stderr.write("You don't have a package.json in this folder")
+			process.exit(0)
+		} else {
+			throw new Error(error)
+		}
+	}
 }
 
 main()
