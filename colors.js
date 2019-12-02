db5c2c1069dc812ba40c288cfee8e0073b90ea0d
Roque Francisco <roquefsd@gmail.com>
2019-12-02 18:24:21 -0200

    v0.3.2

--- colors.js
+++ colors.js
@@ -1,9 +1,9 @@
 const wrap = (a, b, msg) => {
-	return '\u001b[' + a + 'm' + msg + '\u001b[' + b + 'm'
+  return '\u001b[' + a + 'm' + msg + '\u001b[' + b + 'm'
 }
 
 const bold = message => {
-	return wrap(1, 22, message)
+  return wrap(1, 22, message)
 }
 
 module.exports = { bold }
